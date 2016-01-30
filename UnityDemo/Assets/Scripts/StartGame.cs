5f259ee5b2c6a76e9438672df61a1c3f6c91558d
Jamie Scott <github@jami.org.uk>
2016-01-30 19:57:10 +0000

    fixed mouse hover

--- UnityDemo/Assets/Scripts/StartGame.cs
+++ UnityDemo/Assets/Scripts/StartGame.cs
@@ -1,7 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+ using UnityEngine.EventSystems;  
+ using UnityEngine.UI;
 
-public class StartGame : MonoBehaviour {
+public class StartGame : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public Text text;
 
     public void startGame()
     {
@@ -17,5 +22,14 @@ public class StartGame : MonoBehaviour {
     {
         Application.LoadLevel(1);
     }
-	
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        text.fontSize = text.fontSize + 10;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        text.fontSize = text.fontSize - 10;
+    }
 }
