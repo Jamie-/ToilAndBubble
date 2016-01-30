09750a2e234e76f5547cdb341d709114f12f7e41
Jamie Scott <github@jami.org.uk>
2016-01-30 23:53:59 +0000

    updated start screen with title animation

---
+++ UnityDemo/Assets/Scripts/TitleAnimation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleAnimation : MonoBehaviour {
+    
+    private bool direction;
+	
+	// Update is called once per frame
+	void Update () {
+        if (direction) transform.Translate(new Vector2(0, 10) * Time.deltaTime);
+        else transform.Translate(new Vector2(0, 10) * Time.deltaTime * -1);
+
+        if (transform.position.y > 420) direction = false;
+        if (transform.position.y < 400) direction = true;
+
+    }
+}
