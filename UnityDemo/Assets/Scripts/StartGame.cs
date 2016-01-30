07ee6011c4a59f36784d39954d0f4e1b6466c85a
Jamie Scott <github@jami.org.uk>
2016-01-30 19:48:37 +0000

    working start ui

---
+++ UnityDemo/Assets/Scripts/StartGame.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+
+    public void startGame()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+
+    void OnMouseEnter()
+    {
+        Application.LoadLevel(1);
+    }
+	
+}
