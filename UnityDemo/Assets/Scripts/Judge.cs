41c507404472e9249403fb6242213842aed7ffd9
Jamie Scott <github@jami.org.uk>
2016-01-31 05:17:59 +0000

    converted winning script to run in unity

--- UnityDemo/Assets/Scripts/Judge.cs
+++ UnityDemo/Assets/Scripts/Judge.cs
@@ -2,19 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine.UI;
+using UnityEngine;
 
-namespace Assets.Scripts
-{
-    class Judge
+//namespace Assets.Scripts
+//{
+public class Judge : MonoBehaviour
     {
         public static String winnerName;
         public static double leftScore, rightScore;
+        public Text text;
+
+        void Start()
+        {
+            updateStats();
+            text.text = winnerName;
+        }
 
         public static void updateStats()
         {
             leftScore = (360 - getDifference(Cauldron.Hue, LeftPotion.h)) / 360 * 100;
             rightScore = (360 - getDifference(Cauldron.Hue, RightPotion.h)) / 360 * 100;
-            if (leftScore > rightScore) winnerName = "Left"; else winnerName = "Right";
+            Debug.Log(leftScore);
+            Debug.Log(rightScore);
+        if (leftScore == rightScore) winnerName = "Draw";
+        else if (leftScore > rightScore) winnerName = "Left";
+        else winnerName = "Right";
         }
 
         private static double getDifference(double one, double two)
@@ -34,4 +47,4 @@ namespace Assets.Scripts
             return Math.Min(delta1, delta2);
         }
     }
-}
+//}
