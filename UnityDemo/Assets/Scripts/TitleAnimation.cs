3486a02e9b231f4527c8f0c77a76b88128c6db09
Jamie Scott <github@jami.org.uk>
2016-01-31 00:28:52 +0000

    fixed canvas scaling for start and end screens

--- UnityDemo/Assets/Scripts/TitleAnimation.cs
+++ UnityDemo/Assets/Scripts/TitleAnimation.cs
@@ -4,14 +4,16 @@ using System.Collections;
 public class TitleAnimation : MonoBehaviour {
     
     private bool direction;
+    public int Min;
+    public int Max;
 	
 	// Update is called once per frame
 	void Update () {
         if (direction) transform.Translate(new Vector2(0, 10) * Time.deltaTime);
         else transform.Translate(new Vector2(0, 10) * Time.deltaTime * -1);
 
-        if (transform.position.y > 420) direction = false;
-        if (transform.position.y < 400) direction = true;
+        if (transform.localPosition.y > Max) direction = false;
+        if (transform.localPosition.y < Min) direction = true;
 
     }
 }
